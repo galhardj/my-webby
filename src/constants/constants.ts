fca816b2dc74e658f17beb3232a04afce302730f
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-05 22:44:41 +0700

    prettier

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -38,8 +38,9 @@ export const HOMEPAGE_CONTENT = {
 export const BANNER_TWO = {
   image: {
     src: "/images/meetings.png",
-    alt: "Season Sip Banner"
+    alt: "Season Sip Banner",
   },
   title: "Welcome back, Pumpkin Spice Latte",
-  description: "It's the sip you've been waiting for all year. Our Pumpkin Spice Latte brings autumn to life in every cup."
+  description:
+    "It's the sip you've been waiting for all year. Our Pumpkin Spice Latte brings autumn to life in every cup.",
 } as const;

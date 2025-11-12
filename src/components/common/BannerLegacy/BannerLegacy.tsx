64ba049b18264b20936e6b29aa28de4e419c8370
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-13 01:31:11 +0700

    Apply section' padding in globalcss

--- src/components/common/BannerLegacy/BannerLegacy.tsx
+++ src/components/common/BannerLegacy/BannerLegacy.tsx
@@ -3,7 +3,7 @@ import Image from "@/src/components/atomic/atom/Image";
 
 const BannerLegacy = () => {
   return (
-    <section className="mx-auto max-w-5xl px-6 py-12">
+    <section className="mx-auto max-w-5xl">
       <div className="grid gap-10 md:grid-cols-2 md:items-center">
         <Text />
         <Image />

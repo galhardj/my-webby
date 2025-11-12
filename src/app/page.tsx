4cd559712b64bff841e517c6382666e61f21fce1
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-12 22:37:46 +0700

    Folder refactor for common components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import BannerLegacy from "@/src/components/BannerLegacy/BannerLegacy";
-import Footer from "@/src/components/Footer/Footer";
-import Banner from "@/src/components/Banner/Banner";
+import BannerLegacy from "@/src/components/common/BannerLegacy/BannerLegacy";
+import Footer from "@/src/components/common/Footer/Footer";
+import Banner from "@/src/components/common/Banner/Banner";
 import { BANNER } from "@/src/constants/banner";
 
 export default function Page() {

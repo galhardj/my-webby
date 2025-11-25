2dd65870ca0c2021934cdb3a209eeae3a802c9bb
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-25 18:30:38 +0700

    add clear localStorage to Form' button

--- src/components/common/UserInput/organism/Form/Form.tsx
+++ src/components/common/UserInput/organism/Form/Form.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import styles from "./Form.module.css";
 import type { Form } from "@/src/type/prop";
 import ContainerMapper from "./mapper/ContainerMapper";
 
 const Form = ({ title, description, containers, buttonText }: Form) => {
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    localStorage.clear()
+  }
+  
   return (
     <section className={styles["form-section"]}>
       <h2>{title}</h2>
       <p>{description}</p>
-      <form className={styles.form}>
+      <form onSubmit={submitHandler} className={styles.form}>
         <ContainerMapper containers={containers} />
-        <button>{buttonText}</button>
+        <button type="submit">{buttonText}</button>
       </form>
     </section>
   );

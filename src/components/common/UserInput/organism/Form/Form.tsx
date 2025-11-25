6a506db98ed3dc227551058ab7d4daff1f252387
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-25 22:41:37 +0700

    Prettier

--- src/components/common/UserInput/organism/Form/Form.tsx
+++ src/components/common/UserInput/organism/Form/Form.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import styles from "./Form.module.css";
 import type { Form } from "@/src/type/prop";
@@ -6,10 +6,10 @@ import ContainerMapper from "./mapper/ContainerMapper";
 
 const Form = ({ title, description, containers, buttonText }: Form) => {
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    localStorage.clear()
-  }
-  
+    e.preventDefault();
+    localStorage.clear();
+  };
+
   return (
     <section className={styles["form-section"]}>
       <h2>{title}</h2>

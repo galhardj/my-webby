67e2f2e8474f5b94bf1b37c11d35121a5b1b9cd5
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-11-25 16:35:13 +0700

    Implement useEffect (& useState) - to save latest Textarea' content in localStorage

--- src/components/common/UserInput/atomic/common/Textarea.tsx
+++ src/components/common/UserInput/atomic/common/Textarea.tsx
@@ -1,11 +1,37 @@
+"use client";
+
 import type { Textarea } from "@/src/type/prop";
 import Container from "../Container/Container";
+import { useState, useEffect } from "react";
 
 const Textarea = ({ id, label, placeholder }: Textarea) => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const savedMsg = localStorage.getItem("saved-message");
+    if (savedMsg) {
+      setMessage(savedMsg);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("saved-message", message);
+  }, [message]);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <Container>
       <label htmlFor={id}>{label}</label>
-      <textarea id={id} name={id} placeholder={placeholder} />
+      <textarea
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        value={message}
+        onChange={changeHandler}
+      />
     </Container>
   );
 };

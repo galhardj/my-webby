442d8221d0701ce64d71667ba3efc48b661f9527
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-12-24 22:23:48 +0700

    Move SSR fetching to parent' Server component from ProductList

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,29 @@
+import { fetchApi } from "@/src/lib/fetchApi";
+import { SSR } from "@/src/constants/rendering-type";
 import FestiveMenu from "@/src/components/common/Products/ProductsClaude";
 import ProductList from "@/src/components/common/Products/ProductList";
 
-export default function Page() {
+export default async function Page() {
+  const { results: pokemonList }: { results: any[] } = await fetchApi<any>(
+    "https://pokeapi.co/api/v2/pokemon/",
+    SSR,
+  );
+
+  const pokemonDetailList: any[] = await Promise.all(
+    pokemonList.map(async ({ url }: any) => {
+      const pokemonDetail = await fetchApi<any>(url, SSR);
+
+      return {
+        name: pokemonDetail.forms[0].name,
+        image: pokemonDetail.sprites.other.home.front_default,
+      };
+    }),
+  );
+
   return (
     <main className="min-h-dvh">
       <FestiveMenu />
-      <ProductList />
+      <ProductList pokemonList={pokemonDetailList} />
     </main>
   );
 }
--- src/components/common/Products/ProductList.tsx
+++ src/components/common/Products/ProductList.tsx
@@ -1,23 +1,15 @@
-import { fetchApi } from "@/src/lib/fetchApi";
-import { SSR } from "@/src/constants/rendering-type";
 import ProductItem from "./ProductItem";
 
 // TODO: to update types to not use 'any'
-const ProductList = async () => {
-  const pokemonList = await fetchApi<any>(
-    "https://pokeapi.co/api/v2/pokemon/",
-    SSR,
-  ).then((res) => res.results);
-
+const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
   return (
-    <div className="max-w-4xl mx-auto my-8">
+    <div className="mx-auto my-8 max-w-4xl">
       <ul className="grid grid-cols-5 gap-4">
-        {pokemonList.map(async (pokemon: any, index: any) => {
-          const pokemonDetails = await fetchApi<any>(pokemon.url, SSR);
+        {pokemonList.map((pokemon: any, index: any) => {
           return (
             <ProductItem
-              name={pokemonDetails.forms[0].name}
-              image={pokemonDetails.sprites.other.home.front_default}
+              name={pokemon.name}
+              image={pokemon.image}
               key={index}
             />
           );

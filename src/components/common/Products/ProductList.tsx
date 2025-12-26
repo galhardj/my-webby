1cde972782165a911ed0a9c106e05fbe09cfc1d2
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-12-26 19:20:53 +0700

    Refactor ProductList

--- src/components/common/Products/ProductList.tsx
+++ src/components/common/Products/ProductList.tsx
@@ -8,7 +8,7 @@ import Button from "../Button";
 // TODO: to update types to not use 'any'
 const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
   const lengthDiff = 10;
-  const [item, setItem] = useState(lengthDiff);
+  const [itemLen, setItemLen] = useState(lengthDiff);
   const [filterQuery, setFilterQuery] = useState("");
 
   const filteredList = pokemonList.filter((pokemon) =>
@@ -17,7 +17,39 @@ const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
 
   const filterHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilterQuery(e.target.value);
-    setItem(lengthDiff);
+    setItemLen(lengthDiff);
+  };
+
+  const ButtonSection = () => {
+    return (
+      <>
+        {itemLen < filteredList.length ? (
+          <div className="flex gap-3">
+            <Button
+              className="rounded-2xl border bg-amber-300 px-6 py-3 text-gray-600"
+              onClick={() => setItemLen((prev) => prev + lengthDiff)}
+            >
+              Show more
+            </Button>
+            <Button
+              className="rounded-2xl border bg-amber-400 px-9 py-3 text-gray-600"
+              onClick={() => setItemLen(pokemonList.length)}
+            >
+              Show all
+            </Button>
+          </div>
+        ) : filteredList.length > lengthDiff ? (
+          <Button
+            className="rounded-2xl border bg-blue-200 px-9 py-3 text-gray-600"
+            onClick={() => setItemLen(lengthDiff)}
+          >
+            Minimize
+          </Button>
+        ) : (
+          <></>
+        )}
+      </>
+    );
   };
 
   return (
@@ -25,7 +57,7 @@ const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
       <div className="flex flex-col items-center gap-6">
         <SearchBox onFilterChange={filterHandler} />
         <ul className="grid grid-cols-5 gap-4">
-          {filteredList.slice(0, item).map((pokemon: any, index: number) => {
+          {filteredList.slice(0, itemLen).map((pokemon: any, index: number) => {
             return (
               <ProductItem
                 name={pokemon.name}
@@ -35,29 +67,7 @@ const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
             );
           })}
         </ul>
-        {item < filteredList.length ? (
-          <div className="flex gap-3">
-            <Button
-              className="rounded-2xl border bg-amber-300 px-6 py-3 text-gray-600"
-              onClick={() => setItem((prev) => prev + lengthDiff)}
-            >
-              Show more
-            </Button>
-            <Button
-              className="rounded-2xl border bg-amber-400 px-9 py-3 text-gray-600"
-              onClick={() => setItem(pokemonList.length)}
-            >
-              Show all
-            </Button>
-          </div>
-        ) : (
-          <Button
-            className="rounded-2xl border bg-blue-200 px-9 py-3 text-gray-600"
-            onClick={() => setItem(lengthDiff)}
-          >
-            Minimize
-          </Button>
-        )}
+        <ButtonSection />
       </div>
     </div>
   );

0d59265734009cc750b627166e1693178eb35472
Galih Aria Dananjaya <galiharia.d@photon.in>
2025-12-26 18:57:48 +0700

    Refactor ProductList

--- src/components/common/Products/ProductList.tsx
+++ src/components/common/Products/ProductList.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import ProductItem from "./ProductItem";
+import SearchBox from "./SearchBox";
+import Button from "../Button";
 
 // TODO: to update types to not use 'any'
 const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
@@ -13,44 +15,15 @@ const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
     pokemon.name.toLowerCase().includes(filterQuery.toLowerCase()),
   );
 
-  const showMoreHandler = () => setItem((prev) => prev + lengthDiff);
-  const showMore = item < filteredList.length;
+  const filterHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterQuery(e.target.value);
+    setItem(lengthDiff);
+  };
 
-  // TODO: Consider moving SVG import from different file
   return (
     <div className="mx-auto my-8 max-w-4xl">
       <div className="flex flex-col items-center gap-6">
-        <div>
-          <label htmlFor="filter-product" className="sr-only">
-            Search for items
-          </label>
-          <div className="relative">
-            <input
-              id="filter-product"
-              type="text"
-              className="rounded-2xl border-2 px-12 py-6"
-              onChange={(e) => {
-                setFilterQuery(e.target.value);
-                setItem(lengthDiff);
-              }}
-              placeholder="Search pokemon"
-            />
-            <svg
-              className="absolute top-1/2 left-4 h-6 w-6 -translate-y-1/2 transform text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </div>
-        </div>
+        <SearchBox onFilterChange={filterHandler} />
         <ul className="grid grid-cols-5 gap-4">
           {filteredList.slice(0, item).map((pokemon: any, index: number) => {
             return (
@@ -62,13 +35,28 @@ const ProductList = ({ pokemonList }: { pokemonList: any[] }) => {
             );
           })}
         </ul>
-        {showMore && (
-          <button
-            className="rounded-2xl border bg-amber-300 px-6 py-3"
-            onClick={showMoreHandler}
+        {item < filteredList.length ? (
+          <div className="flex gap-3">
+            <Button
+              className="rounded-2xl border bg-amber-300 px-6 py-3 text-gray-600"
+              onClick={() => setItem((prev) => prev + lengthDiff)}
+            >
+              Show more
+            </Button>
+            <Button
+              className="rounded-2xl border bg-amber-400 px-9 py-3 text-gray-600"
+              onClick={() => setItem(pokemonList.length)}
+            >
+              Show all
+            </Button>
+          </div>
+        ) : (
+          <Button
+            className="rounded-2xl border bg-blue-200 px-9 py-3 text-gray-600"
+            onClick={() => setItem(lengthDiff)}
           >
-            Show more
-          </button>
+            Minimize
+          </Button>
         )}
       </div>
     </div>
---
+++ src/components/common/Products/SearchBox.tsx
@@ -0,0 +1,39 @@
+interface SearchBox {
+  onFilterChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+// TODO: Consider moving SVG import from different file
+const SearchBox = ({ onFilterChange }: SearchBox) => {
+  return (
+    <>
+      <label htmlFor="filter-product" className="sr-only">
+        Search for items
+      </label>
+      <div className="relative">
+        <input
+          id="filter-product"
+          type="text"
+          className="rounded-2xl border-2 px-12 py-6"
+          onChange={onFilterChange}
+          placeholder="Search pokemon"
+        />
+        <svg
+          className="absolute top-1/2 left-4 h-6 w-6 -translate-y-1/2 transform text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export default SearchBox;

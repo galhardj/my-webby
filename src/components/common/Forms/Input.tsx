d548e54398fcf78742d88c515a994b8366ae2901
Galih Aria Dananjaya <galiharia.d@photon.in>
2026-01-25 00:59:26 +0700

    Input component

--- src/components/common/Forms/Input.tsx
+++ src/components/common/Forms/Input.tsx
@@ -1,19 +1,26 @@
-import type { Input } from "@/src/type/forms";
+import type { InputProps } from "@/src/type/forms";
+import { forwardRef } from "react";
+import { cn } from "@/src/lib/utils/mergeTailwind";
 
-const Input = ({ id, type, label }: Input) => {
-  return (
-    <div>
-      <label htmlFor={id} className="mb-2 block">
-        {label}
-      </label>
-      <input
-        type={type}
-        id={id}
-        name={id}
-        className="w-full rounded-sm border-2 px-2 py-2.5"
-      />
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, id, children, className, ...props }, ref) => {
+    return (
+      <div>
+        <label htmlFor={id} className="mb-2 block">
+          {label}
+        </label>
+        <input
+          ref={ref}
+          id={id}
+          name={id}
+          {...props}
+          className={cn("w-full rounded-sm border-2 px-2 py-2.5", className)}
+        />
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
 
 export default Input;

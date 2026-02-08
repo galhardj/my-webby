93739ed3dee5fdfd896602195fbce149cf1de798
Galih <galihhad@gmail.com>
2026-02-09 03:03:31 +0700

    collectCoverageFrom jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -16,10 +16,21 @@ const config = {
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.ts",
   },
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   extensionsToTreatAsEsm: [".ts", ".tsx"],
+  collectCoverageFrom: [
+    "src/components/common/**/*.{ts,tsx}",
+    "src/hooks/**/*.{ts,tsx}",
+    "src/lib/**/*.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/**/*.spec.{ts,tsx}",
+    "!src/**/__tests__/**",
+    "!src/**/__mocks__/**",
+    "!src/**/*.d.ts",
+  ],
 };
 
 export default config;

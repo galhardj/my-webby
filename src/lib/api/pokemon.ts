eaf60ae48d262d0755421115d04ef8a3c36363f6
Galih <galihhad@gmail.com>
2026-02-09 00:30:27 +0700

    Change limit pokemon api to 50

--- src/lib/api/pokemon.ts
+++ src/lib/api/pokemon.ts
@@ -4,7 +4,7 @@ import { PokeApi, type PokemonList } from "@/src/type/pokemon";
 
 export const getPokemonList = async (): Promise<PokemonList[]> => {
   const { results } = await fetcher<PokeApi>(
-    "https://pokeapi.co/api/v2/pokemon?limit=150",
+    "https://pokeapi.co/api/v2/pokemon?limit=50",
     SSR,
   );
 

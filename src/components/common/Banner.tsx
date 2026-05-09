a22a484e616862d3e376b748902b1234b950616d
Galih <galihhad@gmail.com>
2026-05-09 14:50:39 +0700

    Styling update for banner

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -3,15 +3,18 @@ import type { Banner } from "@/src/type/content";
 const Banner = ({ position, image, title, description, richText }: Banner) => {
   return (
     <section
-      className={`flex gap-6 ${position === "right" ? "flex-row-reverse" : "flex-row"}`}
+      className={`flex flex-col gap-12 lg:gap-16 md:${position === "right" ? "flex-row-reverse" : "flex-row"}`}
     >
       <figure className="flex-1">
-        <img className="block max-w-full h-auto" src={image.src} alt={image.alt} />
+        <img
+          className="block h-auto max-w-full rounded-md"
+          src={image.src}
+          alt={image.alt}
+        />
       </figure>
-      <div className="flex flex-col justify-center gap-4 flex-1">
-        <h2 className="h2">{title}</h2>
-        <p>{description}</p>
-        {richText}
+      <div className="flex flex-1 flex-col justify-center gap-4 text-justify md:gap-5">
+        <h2>{title}</h2>
+        {richText ? richText : <p>{description}</p>}
       </div>
     </section>
   );

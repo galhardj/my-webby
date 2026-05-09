fee4f317162d47949cbca5a128953686278f1107
Galih <galihhad@gmail.com>
2026-05-09 15:41:19 +0700

    Banner width separation change

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -3,16 +3,16 @@ import type { Banner } from "@/src/type/content";
 const Banner = ({ position, image, title, description, richText }: Banner) => {
   return (
     <section
-      className={`flex flex-col gap-12 lg:gap-16 md:${position === "right" ? "flex-row-reverse" : "flex-row"}`}
+      className={`flex flex-col gap-8 lg:gap-16 md:${position === "right" ? "flex-row-reverse" : "flex-row"}`}
     >
-      <figure className="flex-1">
+      <figure className="md:w-2/5">
         <img
           className="block h-auto max-w-full rounded-md"
           src={image.src}
           alt={image.alt}
         />
       </figure>
-      <div className="flex flex-1 flex-col justify-center gap-4 text-justify md:gap-5">
+      <div className="flex flex-col justify-center gap-4 text-justify md:w-3/5 md:gap-5">
         <h2>{title}</h2>
         {richText ? richText : <p>{description}</p>}
       </div>

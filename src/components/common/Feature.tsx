451beb3e941ceb8b0d00a989f5f670cbda3c1ad7
Galih <galihhad@gmail.com>
2026-05-14 09:59:26 +0700

    Update text section into items-start

--- src/components/common/Feature.tsx
+++ src/components/common/Feature.tsx
@@ -46,7 +46,7 @@ const Feature = ({
           alt={image.alt}
         />
       </figure>
-      <div className="flex flex-col justify-center gap-2 text-justify leading-6 md:w-3/5 md:gap-5">
+      <div className="flex flex-col items-start justify-center gap-2 text-justify leading-6 md:w-3/5 md:gap-5">
         <h2 className="text-left">{title}</h2>
         {richText ? (
           richText
